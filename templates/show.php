28d11a44937d5b3c2b1445d60f1d6a65c59d748a
Robert Costa <zabbarob@gmail.com>
2013-04-08 17:24:19 +0200

    replaced <a> for external link by <input> text field; text in the field is automatically selected; added help text; replaced alert message by inline text message.

--- templates/show.php
+++ templates/show.php
@@ -1,24 +1,29 @@
 <?
 $href = htmlReady($external_link);
+$button_text = _("Link in die Zwischenablage kopieren");
 ?>
 
-Der externe Link zur Veranstaltung ist:<br>
-<a href="<?=$href?>" target="_blank"><?=$href?></a><br>
-<button id="copy-button" data-clipboard-text="<?=$href?>" title="Klicken zum kopieren.">Link in die Zwischenablage kopieren</button>
+<p>Unten sehen sie den externen Link auf die aktuelle Veranstaltung. Benutzer die nicht eingeloggt sind werden nach Aufruf des Links automatisch zum Login und danach auf die Veranstaltungsseite weitergeleitet. Um den externen Link auf die Veranstaltung in die Zwischenablage zu kopieren, klicken sie einfach auf den Knopf "<?=$button_text?>". Alternativ k&ouml;nnen sie auch den Text im Eingabefeld markieren und mit Ctrl-C (Windows), beziehungsweise CMD-C (OS X) kopieren.</p>
+
+<p><label for="external-link">Externer Link:</label>
+<input type="text" id="external-link" name="external-link" value="<?=$href?>" disabled="disabled" size="<?=strlen($href)?>" /><p>
+
+<p><button id="copy-button" data-clipboard-text="<?=$href?>" title="Klicken zum kopieren."><?=$button_text?></button> <span id="copied" style="display:none; color:green;">Der Link wurde kopiert.</span></p>
 
 <script src="/studip/plugins_packages/virtUOS/ExternalLinkPlugin/assets/ZeroClipboard.js"></script>
 
 <script>
-// http://jonrohan.github.io/ZeroClipboard/
-// https://github.com/jonrohan/ZeroClipboard
-var clip = new ZeroClipboard( document.getElementById("copy-button"), {
-  moviePath: "/studip/plugins_packages/virtUOS/ExternalLinkPlugin/assets/ZeroClipboard.swf"
-} );
+link = document.getElementById('external-link');
+link.select();
 
-clip.on( 'complete', function(client, args) {
-    // this = object that has been clicked (copy-button)
-    // args.text = value that has been copied to clipboard
-  alert("Der Link wurde in die Zwischenablage kopiert.");
-} );
+// https://github.com/jonrohan/ZeroClipboard
+var clip = new ZeroClipboard(document.getElementById("copy-button"), {
+  moviePath: "/studip/plugins_packages/virtUOS/ExternalLinkPlugin/assets/ZeroClipboard.swf"
+});
+
+clip.on('complete', function(client, args) {
+    copied = document.getElementById('copied');
+    copied.style.display = 'inline';
+});
 </script>
 
