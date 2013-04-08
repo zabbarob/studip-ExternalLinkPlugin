29e1878ffdcadf607f99ed951376cb2d8b9e0aec
Robert Costa <zabbarob@gmail.com>
2013-04-08 15:45:48 +0200

    cleanup: no longer hiding copy-button after it has been clicked, removed some unneeded lines of code

--- templates/show.php
+++ templates/show.php
@@ -3,11 +3,8 @@ $href = htmlReady($external_link);
 ?>
 
 Der externe Link zur Veranstaltung ist:<br>
-<a href="<?=$href?>" target="_blank"><?=$href?></a>
-
-<br>
-
-<button id="copy-button" data-clipboard-text="<?=$href?>" title="Click to copy me.">Link in die Zwischenablage kopieren</button>
+<a href="<?=$href?>" target="_blank"><?=$href?></a><br>
+<button id="copy-button" data-clipboard-text="<?=$href?>" title="Klicken zum kopieren.">Link in die Zwischenablage kopieren</button>
 
 <script src="/studip/plugins_packages/virtUOS/ExternalLinkPlugin/assets/ZeroClipboard.js"></script>
 
@@ -18,30 +15,10 @@ var clip = new ZeroClipboard( document.getElementById("copy-button"), {
   moviePath: "/studip/plugins_packages/virtUOS/ExternalLinkPlugin/assets/ZeroClipboard.swf"
 } );
 
-clip.on( 'load', function(client) {
-  // alert( "movie is loaded" );
-} );
-
 clip.on( 'complete', function(client, args) {
-  this.style.display = 'none'; // "this" is the element that was clicked
-  alert("Copied text to clipboard: " + args.text );
-} );
-
-clip.on( 'mouseover', function(client) {
-  // alert("mouse over");
-} );
-
-clip.on( 'mouseout', function(client) {
-  // alert("mouse out");
-} );
-
-clip.on( 'mousedown', function(client) {
-
-  // alert("mouse down");
-} );
-
-clip.on( 'mouseup', function(client) {
-  // alert("mouse up");
+    // this = object that has been clicked (copy-button)
+    // args.text = value that has been copied to clipboard
+  alert("Der Link wurde in die Zwischenablage kopiert.");
 } );
 </script>
 
